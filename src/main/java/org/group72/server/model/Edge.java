30b360c9394fcbe054c3412d3ff9e6dbb6268791
Erik Jussi <erikjussi@hotmail.com>
2020-05-22 15:35:19 +0200

    Update Edge.java

--- src/main/java/org/group72/server/model/Edge.java
+++ src/main/java/org/group72/server/model/Edge.java
@@ -41,18 +41,51 @@ public class Edge {
      * as a positive value in meters.
      * @return double distance in meters
      */
-    private static int calculateDistace(double lat1, double lon1, double lat2, double lon2){
-        double radius = 6378.137; // Radius of earth in KM
-        double dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
-        double dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-                        Math.sin(dLon/2) * Math.sin(dLon/2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        double d = radius * c;
-        return (int) d * 1000; // meters
+//    private static int calculateDistace(double lat1, double lon1, double lat2, double lon2, double el1, double el2){
+//        double radius = 6378.137; // Radius of earth in KM
+//        double dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+//        double dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+//        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+//                Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+//                        Math.sin(dLon/2) * Math.sin(dLon/2);
+//        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//        double d = radius * c;
+//        return (int) d * 1000; // meters
+//    }
+
+//    public int calculateDistanceInMeters() {
+//        return (int) Math.round(calculateDistance(getNode1().getLatitude(), getNode1().getLongitude(), getNode2().getLatitude(), getNode2().getLongitude()));
+//    }
+//
+//    public double calculateDistanceRounded() {
+//        return Math.round(calculateDistance(getNode1().getLatitude(), getNode1().getLongitude(), getNode2().getLatitude(), getNode2().getLongitude()) * 100.0) / 100.0;
+//    }
+
+
+    public int calculateDistance() {
+        return (int) Math.round(calculateDistance(getNode1().getLatitude(), getNode1().getLongitude(), getNode2().getLatitude(), getNode2().getLongitude()));
     }
 
+    private double calculateDistance(double lat1, double lng1, double lat2, double lng2) {
+        double earthRadius = 6371000;
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        return earthRadius * c;
+    }
+
+
+//    public int calculateDistanceH() {
+//        return (int) Math.round(calculateDistanceH(getNode1().getLatitude(), getNode1().getLongitude(), getNode2().getLatitude(), getNode2().getLongitude()));
+//    }
+//
+//    private double calculateDistanceH(double lat1, double long1, double lat2, double long2) {
+//        return org.apache.lucene.util.SloppyMath.haversinMeters(lat1, long1, lat2, long2);
+//    }
 
     public Node getNode1() {
         return node1;
