af30a47bac4674a008b41bf51c293164473555c8
sun <empaemanuel@gmail.com>
2020-05-23 22:23:42 +0200

    added CrudRepoLink

--- src/main/java/org/group72/server/model/Route.java
+++ src/main/java/org/group72/server/model/Route.java
@@ -1,5 +1,12 @@
 package org.group72.server.model;
 
+import net.minidev.json.JSONArray;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
 import java.util.UUID;
 /**
 *
@@ -8,17 +15,22 @@ import java.util.UUID;
 *@Author Magnus P.
 * */
 
-@Entity // This tells Hibernate to make a table out of this class
-class Route{
+@Entity
+        // This tells Hibernate to make a table out of this class
+public class Route{
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    @ManyToOne
     private User user;
-    private JSONArray MaxLightRoute;
-    private JSONArray MedLightRoute;
-    private JSONArray MinLightRoute;
+//    private JSONArray MaxLightRoute;
+//    private JSONArray MedLightRoute;
+//    private JSONArray MinLightRoute;
+    @ManyToOne
     private Edge startEdge;
+    @ManyToOne
     private Edge endEdge;
 
     public Route(Edge startEdge, Edge endEdge){
@@ -27,21 +39,24 @@ class Route{
         calculateRoute();
     }
 
+    //Require by JPA
+    public Route(){ }
+
     public long getId() {
         return id   ;
     }
 
-    public JSONArray getMaxLightRoute() {
-        return MaxLightRoute;
-    }
-
-    public JSONArray getMedLightRoute() {
-        return MedLightRoute;
-    }
-
-    public JSONArray getMinLightRoute() {
-        return MinLightRoute;
-    }
+//    public JSONArray getMaxLightRoute() {
+//        return MaxLightRoute;
+//    }
+//
+//    public JSONArray getMedLightRoute() {
+//        return MedLightRoute;
+//    }
+//
+//    public JSONArray getMinLightRoute() {
+//        return MinLightRoute;
+//    }
 
     private void calculateRoute(){
 
