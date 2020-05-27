a1426d7247ef397ba48531b6fec90599bff0a3a1
Max Lindahl <Max.Lindahl97@gmail.com>
2020-05-28 00:52:30 +0200

    more tests

--- src/main/java/org/group72/server/ServerApplication.java
+++ src/main/java/org/group72/server/ServerApplication.java
@@ -2,10 +2,12 @@ package org.group72.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
+@EntityScan(basePackages = "org.group72.server")
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ServerApplication extends SpringBootServletInitializer{
 
