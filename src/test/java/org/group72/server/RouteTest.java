fce81fd960a988a8777f8c46ab701d90700009da
sun <empaemanuel@gmail.com>
2020-05-28 13:41:23 +0200

    updated to JUnit5 and fixed MVc issue

--- src/test/java/org/group72/server/RouteTest.java
+++ src/test/java/org/group72/server/RouteTest.java
@@ -2,12 +2,12 @@ package org.group72.server;
 
 import org.group72.server.controller.RouteController;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(RouteController.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
 public class RouteTest {
 
     @Test
